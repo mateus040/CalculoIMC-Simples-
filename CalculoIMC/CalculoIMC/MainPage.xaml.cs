c1cbe860001dd6d2806896f39b34d8d80ba4ab98
Usuario <Usuario@CPD02-M18>
2022-03-09 14:32:41 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoIMC/CalculoIMC/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculoIMC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double peso = Convert.ToDouble(txt_peso.Text);
+            double altura = Convert.ToDouble(txt_altura.Text);
+
+            double imc = peso / (altura * altura);
+
+            string msg = "Seu IMC é: " + Math.Round(imc,2);
+
+            DisplayAlert("Resultado", msg, "OK");
+
+
+        }
+    }
+}
